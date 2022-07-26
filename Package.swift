4771570dc222ae66850f2e7de51f8840155e6658
jnMars <563438383@qq.com>
2022-07-26 15:50:52 +0800

    Fix - update package

--- Package.swift
+++ Package.swift
@@ -8,16 +8,27 @@ let package = Package(
         .macOS(.v10_14), .iOS(.v10), .tvOS(.v13)
     ],
     products: [
-        .library(
-            name: "Wechat",
-            targets: ["Wechat"])
+        .library(name: "Wechat", targets: ["Wechat"]),
+        .library(name: "WeCom", targets: ["WeCom"]),
+        .library(name: "OneAuth", targets: ["OneAuth"])
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "Wechat",
-            path: "guard-ios-wechat/1.0.2/Wechat.xcframework"
+            url: "https://github.com/Authing/authing-binary/releases/download/wechat-1.0.2/Wechat.xcframework.zip",
+            checksum: "f75ed2933f9e0bad9ab6f497b4835feeb3069eb11bd2faef2193908fd780d98a"
+        ),
+        .binaryTarget(
+            name: "WeCom",
+            url: "https://github.com/Authing/authing-binary/releases/download/wecom-1.0.1/WeCom.xcframework.zip",
+            checksum: "d0722f8c1b132856e19191e5a2f1dbbc52ffd04bbc5267d6ebb3402fcfc64941"
+        ),
+        .binaryTarget(
+            name: "OneAuth",
+            url: "https://github.com/Authing/authing-binary/releases/download/oneauth-1.0.9/OneAuth.xcframework.zip",
+            checksum: "b0e5026e8f5cdd7324c0b2075d7cd5fc9bbdb834d2d267a11c1fe2812c39c3b0"
         )
     ]
 )
