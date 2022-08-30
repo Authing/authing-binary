e958f425081e355240fb1280f9fe0f67af7acde2
jnMars <563438383@qq.com>
2022-08-30 17:05:54 +0800

    Feat - Google binary

--- Package.swift
+++ Package.swift
@@ -18,23 +18,28 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Wechat",
-            url: "https://github.com/Authing/authing-binary/releases/download/1.0.4/Wechat.xcframework.zip",
-            checksum: "a0db2acd9dd5f2847aacc2cb28caa93b5acb79d0d36e2b5c25eb39f096e43566"
+            url: "https://github.com/Authing/authing-binary/releases/download/1.0.5/Wechat.xcframework.zip",
+            checksum: "b5461da4cbfe8d99e0a27489e67fccd1a51201ea2260c370b5b7949766530ad2"
         ),
         .binaryTarget(
             name: "WeCom",
-            url: "https://github.com/Authing/authing-binary/releases/download/1.0.4/WeCom.xcframework.zip",
-            checksum: "c21aceb4c1ee98562f772346d49b95cd23785be3b753303a5af4469ba15db2e6"
+            url: "https://github.com/Authing/authing-binary/releases/download/1.0.5/WeCom.xcframework.zip",
+            checksum: "0aef12e17ae285f07a4b32bd1c8ab1553feab202b7c15c7414aa3435d2c363f3"
         ),
         .binaryTarget(
             name: "OneAuth",
-            url: "https://github.com/Authing/authing-binary/releases/download/1.0.4/OneAuth.xcframework.zip",
-            checksum: "e60cecd6377b852c8d8f649a3c899eae535a3777556f1f013c5d832def1f62c7"
+            url: "https://github.com/Authing/authing-binary/releases/download/1.0.5/OneAuth.xcframework.zip",
+            checksum: "ab2d4352b394dcc6389681caafdaef06b09f663e9cce1f5494b01ff7be27e93b"
         ),
         .binaryTarget(
             name: "LarkLogin",
-            url: "https://github.com/Authing/authing-binary/releases/download/1.0.4/LarkLogin.xcframework.zip",
-            checksum: "a6b3a7dce0d6bb45181079a1785bd2184086e6f21df167dde33b9f9a268a967f"
+            url: "https://github.com/Authing/authing-binary/releases/download/1.0.5/LarkLogin.xcframework.zip",
+            checksum: "03cd73e61a4cab4b2eb9268eeda5052b7f7c49e701e078c62a518d5b8bd0384f"
+        ),
+        .binaryTarget(
+            name: "Google",
+            url: "https://github.com/Authing/authing-binary/releases/download/1.0.5/Google.xcframework.zip",
+            checksum: "7f893d9b4a2ba8d0c78fea1be1268b220c1166193844a843e63249b8682e7727"
         )
     ]
 )
