decbd9164b18d97c864633dafdbbd2fa560b3dac
jnMars <563438383@qq.com>
2023-02-15 15:22:50 +0800

    Version - 1.1.0

--- Package.swift
+++ Package.swift
@@ -20,32 +20,32 @@ let package = Package(
         .binaryTarget(
             name: "Wechat",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/Wechat.xcframework.zip",
-            checksum: "1df227f8a6648dcb697eae7ed988410d787f70465203771f96300904a65fa70b"
+            checksum: "52fe6376541c74730125a194f6ad5bc38cfafcb5cebd74ba21c33e3ce1204ff6"
         ),
         .binaryTarget(
             name: "WeCom",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/WeCom.xcframework.zip",
-            checksum: "e15959f65d96ea4a240c66c59925957f4c86b5695671148a433f69b34d6d97f6"
+            checksum: "f14f1e4bb63f82cb48f9f8b1d4e14c340f7c90577b3a59c983fafbd2609a4d09"
         ),
         .binaryTarget(
             name: "OneAuth",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/OneAuth.xcframework.zip",
-            checksum: "ab2d4352b394dcc6389681caafdaef06b09f663e9cce1f5494b01ff7be27e93b"
+            checksum: "b1f32a867373515765d5a52fd423eb18409358e06618ef340eb071a79db1c4f4"
         ),
         .binaryTarget(
             name: "LarkLogin",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/LarkLogin.xcframework.zip",
-            checksum: "03cd73e61a4cab4b2eb9268eeda5052b7f7c49e701e078c62a518d5b8bd0384f"
+            checksum: "826e59112c7284e594e2ee353dfe9e8db983b98d7ce5226a4f8bea3902eeb655"
         ),
         .binaryTarget(
             name: "Google",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/Google.xcframework.zip",
-            checksum: "7f893d9b4a2ba8d0c78fea1be1268b220c1166193844a843e63249b8682e7727"
+            checksum: "ecad8922805c67697c0f1b08009941d062629c9a5039d4c09d4e5eebe2fb08f6"
         ),
         .binaryTarget(
             name: "Facebook",
             url: "https://github.com/Authing/authing-binary/releases/download/1.1.0/Facebook.xcframework.zip",
-            checksum: "a00c3adc9c2538c2def410a132df98edb87d41366132a5617a969849872ed5dc"
+            checksum: "8484792f41a3be23fd97927d470691de4161d79da384ca106c5348a80aa7e5f6"
         )
     ]
 )
